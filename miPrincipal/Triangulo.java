3e8b5dba84cbf9734b7c37a8eef7b838d2fcd4f2
VICTOR ORTIZ LOPEZ <135753640+ortiz19@users.noreply.github.com>
2024-02-08 15:41:02 +0000

    ULTIMA ACTUALIZACION DE COMENTARIOS

--- miPrincipal/Triangulo.java
+++ miPrincipal/Triangulo.java
@@ -1,22 +1,29 @@
 package miPrincipal;
 
+// Se define la clase Triangulo en el paquete miPrincipal
 public class Triangulo {
-    private Punto v1,v2,v3;
-	//private double longLados[]= {0,0,0};
-	private double longLados[] = new double[3];
-	
-	public Triangulo(Punto v1, Punto v2, Punto v3) {
-		this.v1 = v1;
-		this.v2 = v2;
-		this.v3 = v3;
-	}
-	
-	public double[] longuitudes()
-	{
-		longLados[0]= v1.distancia(v2);
-		longLados[1]= v2.distancia(v3);
-		longLados[2]= v3.distancia(v1);
-		return longLados;
-	}
+    // Variables de instancia para representar los vértices del triángulo
+    private Punto v1, v2, v3;
     
+    // Arreglo para almacenar las longitudes de los lados del triángulo
+    private double longLados[] = new double[3];
+
+    // Constructor que recibe tres puntos que representan los vértices del triángulo
+    public Triangulo(Punto v1, Punto v2, Punto v3) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
+
+    // Método para calcular las longitudes de los lados del triángulo
+    public double[] longuitudes() {
+        // Calcula la longitud de cada lado del triángulo usando el método distancia de la clase Punto
+        longLados[0] = v1.distancia(v2);
+        longLados[1] = v2.distancia(v3);
+        longLados[2] = v3.distancia(v1);
+        
+        // Devuelve el arreglo con las longitudes de los lados
+        return longLados;
+    }
 }
+
